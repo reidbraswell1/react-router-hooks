fa2f4c09490d72ef6c9f3a929ab60075335e1981
reid <reidbras@gmail.com>
2022-09-21 20:42:09 -0500

    Adding index page

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import { HomePage } from "./Home.page.jsx";
+import { FilmsPage } from "./films.page.jsx";
+import { SingleFilmPage } from "./singlefilm.page.jsx";
+
+export { HomePage, FilmsPage, SingleFilmPage };
