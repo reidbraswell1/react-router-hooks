02260c5b04610f7308c717a3812e205faa32223c
reid <reidbras@gmail.com>
2022-09-23 11:31:12 -0500

    Correcting imports

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,6 +1,6 @@
 import { React } from "react";
 import { useState } from "react";
-import Footer from "../components/Footer.jsx";
+import Footer from "../components/footer.jsx";
 
 // Home Page Main Function
 function HomePage(props) {
