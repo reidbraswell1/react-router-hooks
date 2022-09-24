0abecf72ac8772fa1a9f5c9c532282b3304d0b1c
reid <reidbras@gmail.com>
2022-09-24 12:59:35 -0500

    Adding comments

--- src/helpers/film.helpers.js
+++ src/helpers/film.helpers.js
@@ -1,5 +1,7 @@
 import { React } from "react"
 
+// Function that returns a filtered list of films based on
+// a match on a search director.
 function filterFilmsByDirector(searchList,searchDirector) {
 
     console.log(`---Begin filterFilmsByDirector()---`);
@@ -24,7 +26,8 @@ function filterFilmsByDirector(searchList,searchDirector) {
     return filteredList;
 }
 
-
+// Function gets list of specific property from an array
+// of objects. Returns an array of those objects with that value.
 function getListOf(searchList, property) {
 
     console.log(`---Begin Function getListOf()---`);
@@ -45,6 +48,9 @@ function getListOf(searchList, property) {
     return resultArray;
 }
 
+// Film Stats Function - Returns an object with the 
+// total number of films accumulated film total score and
+// the average score.
 function getFilmStats(list) {
 
     console.log(`---Begin Function getFilmStats()---`);
